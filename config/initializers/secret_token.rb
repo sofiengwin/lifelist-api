b91a8d415bf398b87d9ae61facaf1d6e6dcf447d
Ogbara Godwin <godwin.onisofien@andela.com>
2016-04-25 21:46:09 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+LifelistApi::Application.config.secret_token = '70456d66d3e421bbff2b186cf07d9ec752ddf2ae43ce3ceba0e9b28604db011c66ce188c5650933ed7821600811f50b7ac766de9195014be6a695232e4e74789'
