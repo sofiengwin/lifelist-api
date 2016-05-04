48f387375e8dd46a704cec3f8167c5983b006b43
Ogbara Godwin <godwin.onisofien@andela.com>
2016-05-04 12:09:17 +0100

    test(item): add item model tests [Finishes #118802689]

--- spec/models/bucketlist_spec.rb
+++ spec/models/bucketlist_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Bucketlist, type: :model do
+  subject { create(:bucketlist) }
+
+  describe "instance methods" do
+    it { is_expected.to respond_to :name }
+    it { is_expected.to respond_to :user }
+  end
   describe "bucketlist validations" do
     it { should validate_presence_of :name }
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create(:item) }
+
+  describe "instance methods" do
+    it { is_expected.to respond_to :name }
+    it { is_expected.to respond_to :done }
+    it { is_expected.to respond_to :bucketlist }
+  end
+
+  describe "item validations" do
+    it { should validate_presence_of :name }
+  end
+
+  describe "item associations" do
+    it { should belong_to :bucketlist }
+  end
 end
