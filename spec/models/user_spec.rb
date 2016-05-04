33935d937928b24dd0f1cea20052fac8d503a91e
Ogbara Godwin <godwin.onisofien@andela.com>
2016-05-04 11:13:37 +0100

    test(user): add user model test [Starts #118802431]

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create(:user) }
+
+  describe "Instance Methods" do
+    it { is_expected.to respond_to(:name) }
+    it { is_expected.to respond_to(:email) }
+    it { is_expected.to respond_to(:status) }
+  end
+
+  describe "user validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:name) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
+
+  describe "secure password" do
+    it { should have_secure_password }
+  end
+
+  describe "user associations" do
+    it { should have_many(:bucketlists) }
+  end
 end
